d29a6780a538d2571f8bbd290d9c819b30bf8f8e
strican <strican@live.com>
2016-10-01 21:58:04 -0700

    Dictionary Filter
    Check in filter for dictionary to filter a provided dictionary to
    4-letter words. Output is put into the binary's directory, and should be
    copied over as a resource file to the application.

---
+++ DictionaryFilter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDir = System.IO.Directory.GetCurrentDirectory();
+            using (StreamWriter writer = new StreamWriter(currentDir + "\\WordRiverDict.txt"))
+            {
+                using (StreamReader reader = new StreamReader(currentDir + "\\dictionary.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        // Check the contents of the line and write it to our
+                        // final dictionary if it meets the requirements:
+                        //   1. 4 letters lonng
+                        //   2. Not a proper noun
+                        if (line.Length == 4 && char.IsLower(line[0]))
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
